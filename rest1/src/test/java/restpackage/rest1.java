eb5d3384218a902235c3bfc725fe842bb9ea98e0
tuskisharma <tushar235553@gmail.com>
2022-12-07 23:58:15 +0530

    "Rest_Assured"

---
+++ rest1/src/test/java/restpackage/rest1.java
@@ -0,0 +1,36 @@
+package restpackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import io.restassured.path.json.JsonPath;
+import static org.hamcrest.Matchers.*;
+
+import org.json.simple.JSONObject;
+
+public class rest1 {
+public String url="https://reqres.in/api/users?page=2";
+@Test(enabled=false)
+public void tc1() {
+	Response re=RestAssured.get(url);
+	int actual=re.statusCode();
+	Assert.assertEquals(actual, 200);
+}
+@Test(enabled = false)
+public void get_tc2() {
+	given().get(url).then().statusCode(200).log().all();
+	given().get(url).then().statusCode(200).body("data.first_name", hasItems("Michael"));
+}
+@Test
+public void post_tc3() {
+	JSONObject jso=new JSONObject();
+	jso.put("name", "tushar");
+	jso.put("job", "tester");
+	given().body(jso.toJSONString()).when().post("https://reqres.in/api/users").then().statusCode(201).log().all();
+	given().body(jso.toJSONString()).when().put("https://reqres.in/api/users/2").then().statusCode(200).log().all();
+	given().body(jso.toJSONString()).when().post("https://reqres.in/api/register").then().statusCode(200).log().all();
+}
+}
---
+++ rest1/src/test/java/restpackage/trello_boards.java
@@ -0,0 +1,45 @@
+package restpackage;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import io.restassured.path.json.JsonPath;
+import static org.hamcrest.Matchers.*;
+
+public class trello_boards {
+	public String url="https://api.trello.com/";
+	public String keys="840c6ff73133d65cae538c2b01d91f8c";
+	public String token="d0f81b8b8acde55269ae56765ca4d9219236005b92dfb7bfcd4767a39dac075b";
+	public String id;
+
+		
+	@Test(enabled = true)
+	public void create_trello_board() {
+		//JSONObject jr=new JSONObject();
+		RestAssured.baseURI=url;
+		Response response=given().queryParam("name", "iniesta")
+		.queryParam("key", keys)
+		.queryParam("token", token).header("Content-Type", "application/json")
+		.when().contentType(ContentType.JSON).accept(ContentType.JSON).post("/1/boards/").then().assertThat().statusCode(200)
+		.contentType(ContentType.JSON)
+		.extract().response();
+		String jsonresponse = response.asString();
+        JsonPath js= new JsonPath(jsonresponse);
+		id = js.get("id");
+		System.out.println(id);	
+
+		
+	}
+	@Test(enabled = true)
+	public void delete_board() {
+		RestAssured.baseURI=url;
+		given().queryParam("name", "iniesta").queryParam("key", keys)
+		.queryParam("token", token)
+		.when().contentType(ContentType.JSON).accept(ContentType.JSON).delete("/1/boards/"+id).then().statusCode(200).log().all();
+	}
+
+}
